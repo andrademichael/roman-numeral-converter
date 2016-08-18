b43ad4833ffb3de572370bd4b18d0c0b8c17492d
Kevin Johnston and Michael Andrade <kevin@>
2016-08-18 11:57:28 -0700

    add spec 3 and 4 functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   <!--Backend-->
 
-
+  var arabicBases = [1, 5, 10, 50, 100, 500, 1000]
+  var romanBases = ["I", "V", "X", "L", "C", "D", "M"]
   var romanize = function(input) {
     if (!input || input >= 4000) {
       return "Try again.";
-    }  else if () {
-
+    }  else {
+      for (var i = 0; i <= arabicBases.length; i++){
+        if (arabicBases[i] === input) {
+          console.log(arabicBases.length);
+          return romanBases[i];
+        }
+      }
     }
 
     return
