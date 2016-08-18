f83c738a68c3c8e78ea0c273ef4f78e55132b85b
Kevin Johnston and Michael Andrade <kevin@>
2016-08-18 11:24:23 -0700

    Add error catching and fulfills specs 1 & 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
   <!--Backend-->
 
-  <!--Frontend-->
 
+  var romanize = function(input) {
+    if (!input || input >= 4000) {
+      return "Try again.";
+    }  else if () {
+
+    }
+
+    return
+  };
+  <!--Frontend-->
+  $("#converter").submit(function(event) {
+    event.preventDefault();
+    $("#result").text("");
+    var input = parseInt($("#numInput").val());
+    console.log(input);
+    var output = romanize(input);
+    $("#result").text(output);
+  });
 });
